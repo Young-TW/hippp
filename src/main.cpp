7a36c17a6bfab2197559b7c7af98856bb8168d4f
young <young20050727@gmail.com>
2025-05-08 22:30:15 +0800

    feat: move two version vector_add into one file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 // #include "example/vector_add_raii.cpp"
 int vector_add_raii();
+int vector_add_origin();
 
 int main(int argc, char* argv[]) {
     vector_add_raii();
+    vector_add_origin();
     return 0;
 }
