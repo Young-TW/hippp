fda4fb009863b63fa76ce078cf1c7b3bbda0a301
young <young20050727@gmail.com>
2025-05-12 16:40:40 +0800

    feat: define sync() in stream.hpp for hipStreamSynchronize

--- include/hip_raii/stream.hpp
+++ include/hip_raii/stream.hpp
@@ -11,8 +11,11 @@ public:
         hipStreamDestroy(stream_);
     }
 
-    // 重載類型轉換運算子，讓 HipStream 可以直接轉換為 hipStream_t
     operator hipStream_t() const { return stream_; }
+
+    void sync() const {
+        hipStreamSynchronize(stream_);
+    }
 
 private:
     hipStream_t stream_;
