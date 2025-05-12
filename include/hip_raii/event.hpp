a73dc8b66db94335d6424174147679b0a052f74b
young <young20050727@gmail.com>
2025-05-12 16:27:49 +0800

    feat: remove .get() from hipStream and hipEvent
    make all device steps in vector_add_origin are origin HIP syntax

--- include/hip_raii/event.hpp
+++ include/hip_raii/event.hpp
@@ -6,7 +6,9 @@ class HipEvent {
 public:
     HipEvent() { hipEventCreate(&event_); }
     ~HipEvent() { hipEventDestroy(event_); }
-    hipEvent_t get() const { return event_; }
+
+    operator hipEvent_t() const { return event_; }
+
 private:
     hipEvent_t event_;
 };
--- include/hip_raii/stream.hpp
+++ include/hip_raii/stream.hpp
@@ -10,7 +10,10 @@ public:
     ~HipStream() {
         hipStreamDestroy(stream_);
     }
-    hipStream_t get() const { return stream_; }
+
+    // 重載類型轉換運算子，讓 HipStream 可以直接轉換為 hipStream_t
+    operator hipStream_t() const { return stream_; }
+
 private:
     hipStream_t stream_;
 };
