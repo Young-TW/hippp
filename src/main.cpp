dc3e61b57470dee6b53cb40431ef8898259760f8
young <young20050727@gmail.com>
2025-05-22 22:14:42 +0800

    feat!: remove benchmark in src/example (will reimplment in src/benchmark)

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
-int vector_add_raii();
-int vector_add_origin();
+#include <vector>
+
+std::vector<float> vector_add_raii(const std::vector<float>& h_a, const std::vector<float>& h_b);
+std::vector<float> vector_add_origin(const std::vector<float>& h_a, const std::vector<float>& h_b);
 
 int main(int argc, char* argv[]) {
-    vector_add_raii();
-    vector_add_origin();
+    std::vector<float> h_a(1024, 1.0f);
+    std::vector<float> h_b(1024, 2.0f);
+
+    std::vector<float> h_c_raii =  vector_add_raii(h_a, h_b);
+    std::vector<float> h_c_origin = vector_add_origin(h_a, h_b);
     return 0;
 }
