be1b164618de087ac41535926a9f4c4b773fd3f2
young <young20050727@gmail.com>
2025-06-09 17:44:42 +0800

    feat: add matrix_multiplication.h header

---
+++ include/example/device/matrix_multiplication.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <hip/hip_runtime.h>
+
+__global__ void matrix_multiplication(const float* A,
+                                      const float* B,
+                                      float*       C,
+                                      int          M,
+                                      int          N,
+                                      int          K);
