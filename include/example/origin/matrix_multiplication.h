efde12a78deac9b528b9ec8dd1cda3059185b87d
young <young20050727@gmail.com>
2025-06-09 23:40:47 +0800

    feat: add matrix_multiplication.h of origin and raii version header

---
+++ include/example/origin/matrix_multiplication.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <hip/hip_runtime.h>
+#include <vector>
+
+std::vector<float> matrix_multiplication_origin(const std::vector<float>& h_A,
+                                                const std::vector<float>& h_B,
+                                                int M, int N, int K);
