fc509fc5e8e8d7b705f1544b322049c8ba5ae2ae
Young <young20050727@gmail.com>
2025-06-20 00:31:18 +0800

    feat: add headers into hip_raii.hpp

--- include/hip_raii/hip_raii.hpp
+++ include/hip_raii/hip_raii.hpp
@@ -1,6 +1,15 @@
 #pragma once
 
+#include "hip_raii/array.hpp"
 #include "hip_raii/buffer.hpp"
+#include "hip_raii/context.hpp"
 #include "hip_raii/event.hpp"
+#include "hip_raii/graph.hpp"
+#include "hip_raii/graph_executable.hpp"
 #include "hip_raii/host_buffer.hpp"
+#include "hip_raii/kernel.hpp"
+#include "hip_raii/memory_pool.hpp"
+#include "hip_raii/module.hpp"
 #include "hip_raii/stream.hpp"
+#include "hip_raii/surface.hpp"
+#include "hip_raii/texture.hpp"
